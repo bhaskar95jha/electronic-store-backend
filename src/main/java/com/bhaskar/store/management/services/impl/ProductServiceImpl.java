43d2162e711e9a4853ca9af3234674881b940f79
Bhaskar Jha <bhaskarjha@fico.com>
2023-10-22 07:47:43 +0530

    remove unused code

--- src/main/java/com/bhaskar/store/management/services/impl/ProductServiceImpl.java
+++ src/main/java/com/bhaskar/store/management/services/impl/ProductServiceImpl.java
@@ -15,9 +15,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 @Service
 public class ProductServiceImpl implements ProductService {
